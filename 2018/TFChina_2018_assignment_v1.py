4e38b2e9d144a0312502ba0a5e53f76b7c82e87a
Hao Fu <markfu1022@hotmail.com>
2018-04-13 06:50:48 -0700

    Updated input data

--- 2018/TFChina_2018_assignment_v1.py
+++ 2018/TFChina_2018_assignment_v1.py
@@ -14,7 +14,7 @@ pd.options.mode.chained_assignment = None
 ######################################################################################################
 dir_input_file = '2017-2019_20170623104814_cleaned.xlsx'
 dir_mapping_file = 'mapping table.xlsx'
-dir_output_file = 'output.xlsx'
+dir_output_file = '分配结果'.decode('utf-8') + '.xlsx'
 
 ######################################################################################################
 ## LOAD DATA
@@ -40,7 +40,7 @@ str_colname_medical_history = '12.您是否有较严重的过往病史？'.decod
 str_colname_undergrad_school = '8.毕业学校（本科阶段）：'.decode('utf-8')
 str_colname_english_test = '15.您已经完成下列哪类英语水平考试？'.decode('utf-8')
 str_colname_english_score = '16.以上英语考试的成绩为：（例：大学英语四级：570；托福：90）'.decode('utf-8')
-str_colname_degree_category = '10.这个专业属于下列哪一类别：'.decode('utf-8')
+str_colname_degree_category = '6.您高中时期所学科目属于：'.decode('utf-8')
 
 str_colname_prov_name = '省'.decode('utf-8')
 str_colname_prov_ppl_ratio = '人数比例'.decode('utf-8')
@@ -67,19 +67,55 @@ num_frac_has_condition = num_count_has_condition/float(num_count_total_teachers)
 
 ## ratio: is_male
 
+## ratio: has_comb_degree
+num_count_has_comb_degree = len(df_cand[df_cand[str_colname_degree_category] == '综合'.decode('utf-8')]) # need to manually check this column in input data
+num_frac_has_comb_degree = num_count_has_comb_degree/float(num_count_total_teachers)
+
 ## ratio: has_science_degree
 num_count_has_science_degree = len(df_cand[df_cand[str_colname_degree_category] == '理科'.decode('utf-8')]) # need to manually check this column in input data
 num_frac_has_science_degree = num_count_has_science_degree/float(num_count_total_teachers)
 
 ## ratio: can_teach_English
-
+df_cand['can_teach_English'] = [False] * len(df_cand) # create placeholder column
+df_cand[str_colname_english_score] = df_cand[str_colname_english_score].apply(str)
+for idx, row in df_cand.iterrows():
+	str_score = row[str_colname_english_score].decode('utf-8').replace('：', ': ') # clean up strings
+	str_score = str_score.replace('级', '级 ') # clen up strings
+	list_num_score = [int(s) for s in str_score.split() if s.isdigit()] # extract list of integers in string
+	if not list_num_score:
+		num_max_score = 0 # if list is empty, then score is 0 (default)
+	else:
+		num_max_score = max(list_num_score) # if list is not empty, take max score
+	if (
+		('专业八级'.decode('utf-8') in row[str_colname_english_test]) 
+		or ('专业四级'.decode('utf-8') in row[str_colname_english_test]) 
+		or ('GRE' in row[str_colname_english_test]) 
+		or ('SAT' in row[str_colname_english_test]) 
+		or ('专八'.decode('utf-8') in row[str_colname_english_test]) 
+		or ('专四'.decode('utf-8') in row[str_colname_english_test])
+		or ('雅思'.decode('utf-8') in row[str_colname_english_test]) 
+		or ('托福'.decode('utf-8') in row[str_colname_english_test])
+		or (num_max_score >= 500)
+	):
+		df_cand.loc[idx, 'can_teach_English'] = True
+num_count_can_teach_English = len(df_cand[df_cand['can_teach_English']])
+num_frac_can_teach_English = num_count_can_teach_English/float(num_count_total_teachers)
 
 ## calculate each province's total headcount
 df_prov_ratio['headcount_province_total'] = df_prov_ratio[str_colname_prov_ppl_ratio] * num_count_total_teachers
 df_prov_ratio['headcount_province_total'] = df_prov_ratio['headcount_province_total'].apply(round)
 
+## sort province by total headcount
+df_prov_ratio = df_prov_ratio.sort_values(by = 'headcount_province_total', ascending = True)
+
 ## calculate each province's headcount/quota by criteria
-list_criteria = ['has_condition','has_science_degree']
+list_criteria = [
+	'has_condition',
+	'has_comb_degree',
+	'has_science_degree',
+	'can_teach_English'
+]
+
 for c in list_criteria:
 	str_colname_headcount = 'headcount_' + c
 	str_criteria_frac = 'num_frac_' + c
@@ -107,13 +143,46 @@ df_spouse = df_spouse[pd.notnull(df_spouse['spouse_ID'])] # drop null spouse ID
 writer = pd.ExcelWriter(dir_output_file) # initiate output file
 
 def counter_updater(df_top_cand, dict_counter):
+	# has_condition
 	if df_top_cand.loc[0, str_colname_medical_history] == '是'.decode('utf-8'):
 		dict_counter['has_condition'] += 1
+	# is_985211
+	# is_foreign
+	# is_male
+	# has_comb_degree
+	if df_top_cand.loc[0, str_colname_degree_category] == '综合'.decode('utf-8'):
+		dict_counter['has_comb_degree'] += 1
+
+	# has_science_degree
+	if df_top_cand.loc[0, str_colname_degree_category] == '理科'.decode('utf-8'):
+		dict_counter['has_science_degree'] += 1
+
+	# can_teach_English
+	if df_top_cand.loc[0, 'can_teach_English']:
+		dict_counter['can_teach_English'] += 1
+	
 	return dict_counter
 
 def candidate_updater(dict_counter, dict_quota, df_cand_prov):
+	# has_condition
 	if dict_counter['has_condition'] >= dict_quota['has_condition']:
 		df_cand_prov = df_cand_prov[df_cand_prov[str_colname_medical_history] != '是'.decode('utf-8')]
+	# is_985211
+	# is_foreign
+	# is_male
+	
+	# has_comb_degree
+	if dict_counter['has_comb_degree'] >= dict_quota['has_comb_degree']:
+		df_cand_prov = df_cand_prov[df_cand_prov[str_colname_degree_category] != '综合'.decode('utf-8')]
+	
+	# has_science_degree
+	if dict_counter['has_science_degree'] >= dict_quota['has_science_degree']:
+		df_cand_prov = df_cand_prov[df_cand_prov[str_colname_degree_category] != '理科'.decode('utf-8')]
+	
+	# can_teach_English
+	if dict_counter['can_teach_English'] >= dict_quota['can_teach_English']:
+		df_cand_prov = df_cand_prov[~df_cand_prov['can_teach_English']]
+	
 	return df_cand_prov
 
 for p in list(df_prov_ratio[str_colname_prov_name]): # loop through each province
@@ -123,7 +192,7 @@ for p in list(df_prov_ratio[str_colname_prov_name]): # loop through each provinc
 	list_prov_assigned_ID = list() # create placeholder list for assigned candidates to this province
 	df_prov_ratio_sub = df_prov_ratio.loc[df_prov_ratio[str_colname_prov_name] == p] # subset of quota for this province only
 	df_prov_ratio_sub = df_prov_ratio_sub.reset_index()
-	num_prov_quota = df_prov_ratio.loc[df_prov_ratio[str_colname_prov_name] == p, 'headcount_province_total']
+	num_prov_quota = df_prov_ratio.loc[df_prov_ratio[str_colname_prov_name] == p, 'headcount_province_total'] # total headcount for the province
 	
 	# create dictionaries to keep track of all criteria
 	dict_quota = {c: int(df_prov_ratio_sub.loc[0, 'headcount_' + c]) for c in list_criteria} # create dictionary for quota
@@ -161,3 +230,13 @@ for p in list(df_prov_ratio[str_colname_prov_name]): # loop through each provinc
 
 writer.save()
 writer.close()
+
+
+'''
+# for testing purpose only
+writer_temp = pd.ExcelWriter('temp.xlsx')
+df_cand.to_excel(writer_temp, 'full', index = False)
+writer_temp.save()
+writer_temp.close() 
+exit()
+'''
